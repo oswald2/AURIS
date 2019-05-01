0575ae0ff39791043986d12bafe5f317597eac0b
Michael Oswald <michael.oswald@onikudaki.net>
2019-05-01 16:54:01 +0200

    Added also chunks and hexdump for strict byte strings

--- src/General/Chunks.hs
+++ src/General/Chunks.hs
@@ -1,8 +1,14 @@
 module General.Chunks
+    (
+        chunkedBy
+        , chunkedByBS
+        , chunks
+    )
 where
 
 
 import Data.ByteString.Lazy (ByteString)
+import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as B
 
 
@@ -13,6 +19,16 @@ chunkedBy n bs = if B.length bs == 0
   else case B.splitAt (fromIntegral n) bs of
     (as, zs) -> as : chunkedBy n zs
 {-# INLINABLE chunkedBy #-}    
+
+-- | Chunk a @bs into list of smaller byte strings of no more than @n elements
+chunkedByBS :: Int -> BS.ByteString -> [BS.ByteString]
+chunkedByBS n bs = if BS.length bs == 0
+  then []
+  else case BS.splitAt (fromIntegral n) bs of
+    (as, zs) -> as : chunkedByBS n zs
+{-# INLINABLE chunkedByBS #-}    
+
+
 
 -- | divides a list into chunks of sice @n. Last chunk my be smaller
 chunks :: Int -> [a] -> [[a]]
--- src/General/Hexdump.hs
+++ src/General/Hexdump.hs
@@ -3,6 +3,7 @@
 module General.Hexdump 
     (
         hexdump
+        , hexdumpBS
     ) 
 where
     
@@ -12,6 +13,7 @@ import Data.Word
 import Data.Text (Text)
 import Data.Text.Lazy.Builder
 import qualified Data.ByteString.Lazy as BL
+import qualified Data.ByteString as BS
 import Data.List (intersperse)
 import TextShow
 
@@ -20,11 +22,17 @@ import General.Chunks
 import Formatting
 
 
+chunkSize :: Int
+chunkSize = 4
+
+lineChunkSize :: Int
+lineChunkSize = 4
+
+
+-- | hex-dumps the lazy ByteString into a strict Text value
 hexdump :: BL.ByteString -> Text
 hexdump bs = toText result
     where
-        chunkSize = 4
-        lineChunkSize = 4
         blocks = chunkedBy chunkSize bs 
         groups = chunks lineChunkSize blocks
         addresses :: [Word32]
@@ -57,3 +65,40 @@ hexdump bs = toText result
             in
                 content
 
+
+
+-- | hex-dumps the lazy ByteString into a strict Text value
+hexdumpBS :: BS.ByteString -> Text
+hexdumpBS bs = toText result
+    where
+        blocks = chunkedByBS chunkSize bs 
+        groups = chunks lineChunkSize blocks
+        addresses :: [Word32]
+        addresses = [0, fromIntegral (chunkSize * lineChunkSize) .. ]
+
+        result = mconcat $ zipWith groupToLine addresses groups
+
+        byteToHex :: Word8 -> Builder
+        byteToHex x = fromLazyText $ format (left 2 '0' %. hex) x
+
+        byteToChar :: Word8 -> Builder
+        byteToChar x = 
+            let c = chr (fromIntegral x)
+            in 
+            if isPrint c 
+                then singleton c
+                else singleton '.'
+
+        chunkToText = mconcat . map byteToHex . BS.unpack 
+        chunkToPrintable = mconcat . map byteToChar . BS.unpack
+
+        groupToLine :: Word32 -> [BS.ByteString] -> Builder
+        groupToLine addr line = 
+            let content = 
+                    fromLazyText (format (left 8 '0' %. hex % ": ") addr)
+                        <> mconcat (intersperse (singleton ' ') (map chunkToText line))
+                        <> fromLazyText "   "
+                        <> mconcat (map chunkToPrintable line)
+                        <> singleton '\n'
+            in
+                content
