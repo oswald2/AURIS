0f68897d41523734fa16b228e6fe5c9d1aff5459
Michael Oswald <michael.oswald@onikudaki.net>
2019-05-27 22:29:49 +0200

    First version of extraction chain per virtual channel

--- src/Data/PUS/TMFrameExtractor.hs
+++ src/Data/PUS/TMFrameExtractor.hs
@@ -10,10 +10,11 @@ where
 
 import           RIO
 import qualified RIO.ByteString                as B
-import qualified Data.IntMap.Strict            as M
+-- import qualified Data.IntMap.Strict            as M
 
 import           Conduit
 import           Data.Conduit.Attoparsec
+import           Data.Conduit.TQueue
 
 import           Control.PUS.Classes
 
@@ -21,6 +22,7 @@ import           Data.PUS.TMFrame
 import           Data.PUS.PUSPacket
 import           Data.PUS.Types
 import           Data.PUS.PUSPacket
+import           Data.PUS.Config
 
 import           Protocol.ProtocolInterfaces
 
@@ -30,18 +32,57 @@ import           Protocol.ProtocolInterfaces
 -- emptyVCMap = M.empty
 
 
+
+-- tmFrameSwitchVC
+--     :: (MonadIO m, MonadReader env m, HasGlobalState env)
+--     => ConduitT TMFrame TMFrame m ()
+-- tmFrameSwitchVC = do
+--     st <- ask
+--     let vcids = cfgVCIDs (st ^. getConfig)
+--     queues <- mapM (\_ -> newTBQueueIO 1000)
+--     let map = M.fromList $ zipWith f vcids queues
+--         f vcid queue = (fromIntegral (getVCID vcid), queue)
+--         entangledPair 
+
+
+
+tmFrameExtractionChain
+    :: (MonadIO m, MonadReader env m, HasGlobalState env) => 
+    TBQueue TMFrame
+    -> ProtocolInterface
+    -> ConduitT
+           i
+           (Either ParseError (PositionRange, ProtocolPacket PUSPacket))
+           m
+           ()
+tmFrameExtractionChain queue interf =
+    sourceTBQueue queue .| extractPktFromTMFramesC .| pusPacketDecodeC interf
+
+
 extractPktFromTMFramesC
     :: (MonadIO m, MonadReader env m, HasGlobalState env)
     => ConduitT TMFrame ByteString m ()
-extractPktFromTMFramesC = awaitForever $ \frame -> do
-    let (prev, rest) = tmFrameGetPrevAndRest frame
-        -- vcid         = frame ^. tmFrameHdr . tmFrameVcID
-    if B.null prev then yield prev else yield rest
+extractPktFromTMFramesC = go True
+  where
+    go initial = do
+        x <- await
+        case x of
+            Nothing    -> pure ()
+            Just frame ->
+                    -- vcid         = frame ^. tmFrameHdr . tmFrameVcID
+                          if initial
+                then do
+                    let (prev, rest) = tmFrameGetPrevAndRest frame
+                    if B.null prev then yield prev else yield rest
+                    go False
+                else do
+                    yield (frame ^. tmFrameData)
+                    go False
 
 
 pusPacketDecodeC
-    :: (MonadReader env m, HasMissionSpecific env) => 
-    ProtocolInterface
+    :: (MonadReader env m, HasMissionSpecific env)
+    => ProtocolInterface
     -> ConduitT
            ByteString
            (Either ParseError (PositionRange, ProtocolPacket PUSPacket))
