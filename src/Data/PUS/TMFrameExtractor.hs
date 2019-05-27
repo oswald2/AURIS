47e49612ae9bea8d59a794311f8b1e2018a7acf1
Michael Oswald <michael.oswald@onikudaki.net>
2019-05-27 21:59:56 +0200

    Made TM Frame extractor compile clean

--- src/Data/PUS/TMFrameExtractor.hs
+++ src/Data/PUS/TMFrameExtractor.hs
@@ -4,26 +4,25 @@
     , NoImplicitPrelude
 #-}
 module Data.PUS.TMFrameExtractor
-    (
-        extractPktFromTMFramesC
+    ( extractPktFromTMFramesC
     )
 where
 
 import           RIO
-import qualified RIO.ByteString as B
-import qualified Data.IntMap.Strict as M
+import qualified RIO.ByteString                as B
+import qualified Data.IntMap.Strict            as M
 
 import           Conduit
-import Data.Conduit.Attoparsec
+import           Data.Conduit.Attoparsec
 
-import Control.PUS.Classes
+import           Control.PUS.Classes
 
 import           Data.PUS.TMFrame
 import           Data.PUS.PUSPacket
 import           Data.PUS.Types
 import           Data.PUS.PUSPacket
 
-import Protocol.ProtocolInterfaces
+import           Protocol.ProtocolInterfaces
 
 -- type VCMap = M.IntMap ByteString
 
@@ -31,15 +30,23 @@ import Protocol.ProtocolInterfaces
 -- emptyVCMap = M.empty
 
 
-extractPktFromTMFramesC :: (MonadIO m, MonadReader env m, HasGlobalState env) => 
-    ConduitT TMFrame ByteString m ()
+extractPktFromTMFramesC
+    :: (MonadIO m, MonadReader env m, HasGlobalState env)
+    => ConduitT TMFrame ByteString m ()
 extractPktFromTMFramesC = awaitForever $ \frame -> do
     let (prev, rest) = tmFrameGetPrevAndRest frame
         -- vcid         = frame ^. tmFrameHdr . tmFrameVcID
     if B.null prev then yield prev else yield rest
 
 
-pusPacketDecodeC :: ProtocolInterface -> ConduitT ByteString (Either ParseError (PositionRange, PUSPacket)) m ()
-pusPacketDecodeC interf = do 
+pusPacketDecodeC
+    :: (MonadReader env m, HasMissionSpecific env) => 
+    ProtocolInterface
+    -> ConduitT
+           ByteString
+           (Either ParseError (PositionRange, ProtocolPacket PUSPacket))
+           m
+           ()
+pusPacketDecodeC interf = do
     missionSpecific <- view getMissionSpecific
     conduitParserEither (pusPktParser missionSpecific interf)
