e02fceb1677ebd246c9852c51c075432be2d1ce1
Michael Oswald <michael.oswald@onikudaki.net>
2019-06-10 01:20:19 +0200

    First attempt for correct packet extraction, not complete and not working

--- src/Data/PUS/TMFrameDfh.hs
+++ src/Data/PUS/TMFrameDfh.hs
@@ -1,15 +1,34 @@
 {-# LANGUAGE BangPatterns
     , OverloadedStrings
+    , TemplateHaskell
 #-}
 module Data.PUS.TMFrameDfh
     (
         TMFrameDataFieldHeader(..)
         , tmFrameDfhLength
+        , tmfDfhVersion
+        , tmfDfhLength 
+        , tmfDfhVCCont 
+
     )
 where
 
+import RIO
+import Control.Lens (makeLenses)
 
-data TMFrameDataFieldHeader = TMFrameDataFieldHeader
+
+
+data TMFrameDataFieldHeader = 
+    TMFrameEmptyDFH
+    | TMFrameStdHeader {
+        _tmfDfhVersion :: Word8
+        , _tmfDfhLength :: Word8
+        , _tmfDfhVCCont :: Word32 
+    }
+makeLenses ''TMFrameDataFieldHeader
+
+
 
 tmFrameDfhLength :: TMFrameDataFieldHeader -> Int 
-tmFrameDfhLength _ = 0
+tmFrameDfhLength TMFrameEmptyDFH = 0
+tmFrameDfhLength TMFrameStdHeader {} = 4
