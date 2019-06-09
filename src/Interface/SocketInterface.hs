bdfee9a094f5155bf0c1977e9ee4376d4dbc15fe
Michael Oswald <michael.oswald@onikudaki.net>
2019-06-09 17:37:00 +0200

    Corrected interface parsing/sending for the currently available 3 types

--- src/Interface/SocketInterface.hs
+++ src/Interface/SocketInterface.hs
@@ -28,8 +28,8 @@ import           Data.Conduit.Binary           as CB
 import           Data.Conduit.Network
 import           Data.Conduit.TMChan
 import           Data.Word8
-import           Data.Binary                    ( decodeOrFail )
-import           Data.Aeson                     ( eitherDecode )
+import           Data.Binary                    ( decodeOrFail, encode )
+import           Data.Aeson                     ( eitherDecode, encode )
 
 import           Data.PUS.Events
 import           Data.PUS.Config
@@ -44,7 +44,10 @@ data QuitException = QuitException
 
 instance Exception QuitException where
 
-
+-- | Determines the type of the message exchange on the interface
+-- Can be either via Haskell Strings (Read, Show instances) when
+-- 'InterfaceString', via Binary generated structures when 'InterfaceBinary'
+-- and via JSON (line based) when 'InterfaceJSON'
 data InterfaceType =
     InterfaceString
     | InterfaceBinary
@@ -52,119 +55,118 @@ data InterfaceType =
     deriving (Eq, Ord, Enum, Show, Read)
 
 
+-- | The internal server type
 data Server = Server {
     eventChan :: TVar (Maybe (TMChan Event))
 }
 
-
+-- | Returns true if there is currently a connection
 hasConnection :: (MonadIO m) => Server -> m Bool
 hasConnection server = do
     res <- readTVarIO (eventChan server)
     return $ isJust res
 
 
-
+-- | Initializes the socket interface, starts the necessary threads
+-- and listens on the socket for incoming connections
+-- Returns an 'Interface' which should be added to the available
+-- interfaces 
 initSocketInterface
-    :: ( MonadIO m
-       , MonadUnliftIO m
-       , MonadReader env m
-       , HasLogFunc env
-       )
+    :: (MonadIO m, MonadUnliftIO m, MonadReader env m, HasLogFunc env)
     => Config
     -> InterfaceType
     -> Interface
     -> m Interface
-initSocketInterface config ifType interface = do
-    case cfgInterfacePort config of
-        Nothing   -> return interface
-        Just port -> do
-            var <- newTVarIO Nothing
+initSocketInterface config ifType interface = case cfgInterfacePort config of
+    Nothing   -> return interface
+    Just port -> do
+        var <- newTVarIO Nothing
 
-            let newInterface = addInterface interface (localRaiseEvent server)
-                server       = Server { eventChan = var }
+        let newInterface = addInterface interface (localRaiseEvent server)
+            server       = Server { eventChan = var }
 
-            void $ async $ initServer ifType port server newInterface
+        void $ async $ initServer ifType port server newInterface
 
-            return newInterface
+        return newInterface
 
 
 
 localRaiseEvent :: (MonadIO m) => Server -> Event -> m ()
-localRaiseEvent server ev = do
-    atomically $ do
-        res <- readTVar (eventChan server)
-        case res of
-            Just chan -> writeTMChan chan ev
-            Nothing   -> return ()
+localRaiseEvent server ev = atomically $ do
+    res <- readTVar (eventChan server)
+    case res of
+        Just chan -> writeTMChan chan ev
+        Nothing   -> return ()
 
 
 
 initServer
-    :: ( MonadIO m
-       , MonadUnliftIO m
-       , MonadReader env m
-       , HasLogFunc env
-       )
+    :: (MonadIO m, MonadUnliftIO m, MonadReader env m, HasLogFunc env)
     => InterfaceType
     -> Word16
     -> Server
     -> Interface
     -> m ()
-initServer ifType port server interface = do
+initServer ifType port server interface =
     runGeneralTCPServer (serverSettings (fromIntegral port) "*") $ \app -> do
         chan <- liftIO $ newTMChanIO
         atomically $ writeTVar (eventChan server) (Just chan)
         race_
             (runConduit
                 (  appSource app
-                .| C.filterE (/= _cr)
-                .| CB.lines
                 .| readConduit ifType interface
                 .| actionSink interface
                 )
             )
-            (runConduit (sourceTMChan chan .| showConduit .| appSink app))
+            (runConduit (sourceTMChan chan .| showConduit ifType .| appSink app))
         -- on closing the connection, set the Chan back to Nothing
         atomically $ writeTVar (eventChan server) Nothing
 
 
-parseAction :: InterfaceType -> ByteString -> Either Text Action
-parseAction InterfaceString bs =
-    let res = reads (BC.unpack bs)
-    in  if null res
-            then
+parseAction
+    :: (Monad m)
+    => InterfaceType
+    -> ConduitT ByteString (Either Text Action) m ()
+parseAction InterfaceString = C.filterE (/= _cr) .| CB.lines .| proc
+  where
+    proc = awaitForever $ \bs -> do
+        let res = reads (BC.unpack bs)
+        if null res
+            then do
                 let msg = case decodeUtf8' bs of
                         Left  err -> T.pack (show err)
                         Right m   -> m
-                in  Left msg
-            else Right $ fst (L.head res)
-parseAction InterfaceBinary bs = case decodeOrFail (BL.fromStrict bs) of
-    Left  (_, _, err) -> Left (T.pack err)
-    Right (_, _, x  ) -> Right x
-parseAction InterfaceJSON bs = case eitherDecode (BL.fromStrict bs) of
-    Left  err -> Left (T.pack err)
-    Right a   -> Right a
+                yield (Left msg)
+            else yield (Right (fst (L.head res)))
+parseAction InterfaceBinary = awaitForever $ \bs -> do
+    case decodeOrFail (BL.fromStrict bs) of
+        Left  (_, _, err) -> yield (Left (T.pack err))
+        Right (_, _, x  ) -> yield (Right x)
+parseAction InterfaceJSON = C.filterE (/= _cr) .| CB.lines .| proc
+  where
+    proc = awaitForever $ \bs -> do
+        case eitherDecode (BL.fromStrict bs) of
+            Left  err -> yield (Left (T.pack err))
+            Right a   -> yield (Right a)
 
 
 readConduit
-    :: ( MonadIO m
-       , MonadUnliftIO m
-       , MonadReader env m
-       , HasLogFunc env
-       )
+    :: (MonadIO m, MonadUnliftIO m, MonadReader env m, HasLogFunc env)
     => InterfaceType
     -> Interface
     -> ConduitT ByteString Action m ()
-readConduit ifType interface = awaitForever $ \bs -> do
-    case parseAction ifType bs of
-        Left err ->
-            liftIO $ ifRaiseEvent interface (EVAlarms (EV_IllegalAction err))
-        Right action' -> case action' of
-            ActionQuit -> do
-                logWarn
-                    "Received QUIT from Socket Interface, shutting down socket connection..."
-                throwIO QuitException
-            action -> executeAction interface action
+readConduit ifType interface = parseAction ifType .| proc
+  where
+    proc = awaitForever $ \res -> 
+        case res of
+            Left err -> liftIO
+                $ ifRaiseEvent interface (EVAlarms (EV_IllegalAction err))
+            Right action' -> case action' of
+                ActionQuit -> do
+                    logWarn
+                        "Received QUIT from Socket Interface, shutting down socket connection..."
+                    throwIO QuitException
+                action -> yield action
 
 
 
@@ -174,5 +176,10 @@ actionSink interface =
 
 
 
-showConduit :: (Monad m) => ConduitT Event ByteString m ()
-showConduit = awaitForever $ \ev -> yield (BC.pack ((show ev) ++ "\n"))
+showConduit :: (Monad m) => InterfaceType -> ConduitT Event ByteString m ()
+showConduit InterfaceString = awaitForever $ \ev -> do
+    yield (BC.pack ((show ev) ++ "\n"))
+showConduit InterfaceBinary = awaitForever $ \ev -> do 
+    yield (BL.toStrict (Data.Binary.encode ev))
+showConduit InterfaceJSON = awaitForever $ \ev -> do 
+    yield (BL.toStrict (Data.Aeson.encode ev) `BC.snoc` '\n')
