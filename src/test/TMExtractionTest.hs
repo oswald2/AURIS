d8a28eadcc57856668dbd69a3c595508c2db563e
Michael Oswald <michael.oswald@onikudaki.net>
2019-08-13 21:46:02 +0200

    Refined skeleton for TM extraction tests

--- src/test/TMExtractionTest.hs
+++ src/test/TMExtractionTest.hs
@@ -7,13 +7,28 @@ module Main where
 
 
 import           RIO
+import qualified RIO.ByteString                as B
+import qualified RIO.Text                      as T
+import qualified Data.Text.IO                  as T
+import           RIO.List
 
+import           Control.Lens                   ( (.~) )
 
 import           Data.PUS.TMFrame
 import           Data.PUS.TMFrameExtractor
 import           Data.PUS.Config
+import           Data.PUS.MissionSpecific.Definitions
+import           Data.PUS.ExtractedDU
+import           Data.PUS.Types
+import           Data.PUS.GlobalState
+
+import           General.Types
 import           General.Chunks
+
 import           Conduit
+import qualified Data.Conduit.List             as C
+
+import           Protocol.ProtocolInterfaces
 
 import           Test.Hspec
 
@@ -30,7 +45,7 @@ makeTMFrames cfg missionSpecific hdr pl =
     let len       = tmFrameMaxDataLen cfg missionSpecific hdr
         frameData = chunkedByBS len pl
         hdrs      = map upd [0 ..]
-        upd x = hdr { _tmFrameVCFC = x, _tmFrameMCFC = x }
+        upd x = hdr & tmFrameVCFC .~ x & tmFrameMCFC .~ x
         frames = zipWith makeTMFrame hdrs frameData
     in  frames
 
@@ -43,18 +58,30 @@ main = hspec $ do
     describe "TM Frame Extraction" $ do
         it "good extraction" $ do
             let frames = map ep $ makeTMFrames cfg
-                                      defaultMissionSpecific
-                                      tmFrameDefaultHeader
-                                      payload
-                ep x = ExtractedDU {
-                    _epQuality = toFlag Good True,
-                    _epGap = Nothing,
-                    _epSource = IF_NCTRS,
-                    _epDU = x
-                    }
+                                               defaultMissionSpecific
+                                               tmFrameDefaultHeader
+                                               payload
+                ep x = ExtractedDU { _epQuality = toFlag Good True
+                                   , _epGap     = Nothing
+                                   , _epSource  = IF_NCTRS
+                                   , _epDU      = x
+                                   }
                 conduit =
-                    sourceList frames .| extractPktFromTMFramesC defaultMissionSpecific IF_NCTRS .| sinkList
-            res <- runConduit conduit
-            print res
+                    C.sourceList frames
+                        .| extractPktFromTMFramesC defaultMissionSpecific
+                                                   IF_NCTRS
+                        .| C.consume
+            defLogOptions <- logOptionsHandle stdout True
+            let logOptions = setLogMinLevel LevelError defLogOptions
+            withLogFunc logOptions $ \logFunc -> do
+                state <- newGlobalState
+                    defaultConfig
+                    defaultMissionSpecific
+                    logFunc
+                    (\ev -> T.putStrLn ("Event: " <> T.pack (show ev)))
+
+                runRIO state $ do
+                    res <- runConduit conduit
+                    liftIO $ T.putStrLn $ T.pack (show res)
             return ()
 
