4be5cd5dfffb7c7db7d1d0ef834682d69d0306e2
Michael Oswald <michael.oswald@onikudaki.net>
2020-06-07 21:03:07 +0200

    Adjusted background color, looking for suitable axis scaling.

--- auris-tm-displays/src/GUI/Graph.hs
+++ auris-tm-displays/src/GUI/Graph.hs
@@ -45,6 +45,9 @@ import           Data.Text.Short                ( ShortText )
 
 --import           Data.Thyme.Clock
 import qualified Data.Time.Clock               as TI
+import           Data.Time.Calendar
+
+import Data.Colour.SRGB
 
 import           Data.TM.Parameter
 import           Data.TM.Value
@@ -80,15 +83,6 @@ import           GUI.PopupMenu
 type GraphVal = (TI.UTCTime, Double)
 
 
--- instance Eq GraphVal where
---   --g1 == g2 = _graphValTime g1 == _graphValTime g2
---   (t1, _) == (t2 , _) = t1 == t2
-
--- instance Ord GraphVal where
---   --compare g1 g2 = compare (_graphValTime g1) (_graphValTime g2)
---   compare (t1, _) (t2, _) = compare t1 t2
-
-
 -- | Defines a plot. For the parameter with the given 
 -- name, the 'LineStyle' and 'PointStyle' are assigned, as well as the individual
 -- plot values contained in a 'MultiSet', so that they are time-ordered
@@ -100,10 +94,35 @@ data PlotVal = PlotVal {
 }
 makeLenses ''PlotVal
 
+
+data TimeScaleSettings = TimeScaleSettings {
+  _tssInterval :: !TI.NominalDiffTime
+  , _tssOldMin :: !TI.UTCTime
+  , _tssOldMax :: !TI.UTCTime
+}
+makeLenses ''TimeScaleSettings
+
+timeScaleSettings now =
+  let range = TI.secondsToNominalDiffTime 60
+  in  TimeScaleSettings { _tssInterval = range
+                        , _tssOldMin   = now
+                        , _tssOldMax   = TI.addUTCTime range now
+                        }
+
+defaultTimeScaleSettings =
+  let !range = TI.secondsToNominalDiffTime 60
+      !defTime = TI.UTCTime (ModifiedJulianDay 0) (TI.secondsToDiffTime 0)
+  in  TimeScaleSettings { _tssInterval = range
+                        , _tssOldMin   = defTime
+                        , _tssOldMax   = TI.addUTCTime range defTime
+                        }
+
+
 data Graph = Graph {
   _graphName :: !Text
   , _graphParameters :: HashSet ShortText
   , _graphData :: Map ShortText PlotVal
+  , _graphTimeAxisSettings :: TimeScaleSettings
   }
 makeLenses ''Graph
 
@@ -112,7 +131,8 @@ getParameterNames g = toList (g ^. graphParameters)
 
 
 emptyGraph :: Text -> Graph
-emptyGraph name = Graph name HS.empty M.empty
+emptyGraph name = Graph name HS.empty M.empty defaultTimeScaleSettings
+
 
 
 data GraphWidget = GraphWidget {
@@ -126,7 +146,7 @@ makeLenses ''GraphWidget
 
 
 graphWidgetGetParamNames :: TVar Graph -> IO [ShortText]
-graphWidgetGetParamNames var = getParameterNames <$> readTVarIO var 
+graphWidgetGetParamNames var = getParameterNames <$> readTVarIO var
 
 graphWidgetSetChartName :: TVar Graph -> Text -> IO ()
 graphWidgetSetChartName var name = do
@@ -147,12 +167,13 @@ setupGraphWidget parent title paramSelector = do
 
   offscreen <- flcCreateOffscreen (Size (Width (w * 2)) (Height (h * 2)))
 
-  widget' <- widgetCustom
+  widget'   <- widgetCustom
     rect
     Nothing
     (drawChart var offscreen)
-    defaultCustomWidgetFuncs { handleCustom = Just (handleMouse var paramSelector)
-                             }
+    defaultCustomWidgetFuncs
+      { handleCustom = Just (handleMouse var paramSelector)
+      }
 
   end parent
   showWidget widget'
@@ -386,19 +407,16 @@ insertInGraph graph (name, lineStyle, pointStyle) =
 insertParamValue :: Graph -> TMParameter -> Graph
 insertParamValue g@Graph {..} param =
   let paramName = param ^. pName
-  in
-    case M.lookup paramName _graphData of
-      Nothing -> g
-      Just plotVal ->
-        let
-          val =
-            --GraphVal (toUTCTime (param ^. pTime)) (toDouble (param ^. pValue))
-            (toUTCTime (param ^. pTime), toDouble (param ^. pValue))
-          newSet     = MS.insert val (plotVal ^. plotValValues)
-          newPlotVal = plotVal & plotValValues .~ newSet
-          newMap     = M.insert paramName newPlotVal _graphData
-        in
-          g & graphData .~ newMap
+  in  case M.lookup paramName _graphData of
+        Nothing -> g
+        Just plotVal ->
+          let val =
+                --GraphVal (toUTCTime (param ^. pTime)) (toDouble (param ^. pValue))
+                  (toUTCTime (param ^. pTime), toDouble (param ^. pValue))
+              newSet     = MS.insert val (plotVal ^. plotValValues)
+              newPlotVal = plotVal & plotValValues .~ newSet
+              newMap     = M.insert paramName newPlotVal _graphData
+          in  g & graphData .~ newMap
 
 
 plotValToPlot :: PlotVal -> Plot TI.UTCTime Double
@@ -413,7 +431,7 @@ plotValToPlot PlotVal {..} =
     .~ ST.unpack _plotValName
     $  def
   where --values = map (\(GraphVal t p) -> (t, p)) . MS.toList $ _plotValValues
-    values = MS.toList $ _plotValValues
+        values = MS.toList $ _plotValValues
 
 
 titleStyle :: FontStyle
@@ -452,18 +470,42 @@ layoutAxis =
     .~ opaque white
 
 
-graphLayout :: Layout TI.UTCTime Double
-graphLayout =
+layoutTimeAxis :: TimeScaleSettings -> LayoutAxis TI.UTCTime
+layoutTimeAxis settings =
+  def
+    &  laxis_style
+    .~ axisStyle
+    &  laxis_title_style
+    .  font_color
+    .~ opaque white
+    -- &  laxis_generate
+    -- .~ timeAxisFn settings
+
+
+-- timeAxisFn :: TimeScaleSettings -> AxisFn a
+-- timeAxisFn settings xvals = 
+--   timeValueAxis seconds seconds (formatTime defaultTimeLocale "%Ss") UnderTicks minutes (ft "%d-%b-%y %H:%M") BetweenTicks pts
+--   where 
+--     t0 = minimum xvals 
+--     t1 = maximum xvals
+--     start = truncateMinute t1 
+
+
+
+graphLayout :: TimeScaleSettings -> Layout TI.UTCTime Double
+graphLayout settings =
+  let graphBgColor = sRGB24 0x29 0x3d 0x5d
+  in
   layout_background
-    .~ FillStyleSolid (opaque black)
+    .~ FillStyleSolid (opaque graphBgColor)
     $  layout_plot_background
-    ?~ FillStyleSolid (opaque black)
+    ?~ FillStyleSolid (opaque graphBgColor)
     $  layout_title_style
     .~ titleStyle
     $  layout_legend
     ?~ legendStyle
     $  layout_x_axis
-    .~ layoutAxis
+    .~ layoutTimeAxis settings
     $  layout_y_axis
     .~ layoutAxis
     $  def
@@ -473,7 +515,11 @@ chart Graph {..} = toRenderable layout
  where
   plots = map (plotValToPlot . snd) $ M.toList _graphData
   layout =
-    graphLayout & layout_title .~ T.unpack _graphName & layout_plots .~ plots
+    graphLayout _graphTimeAxisSettings
+      &  layout_title
+      .~ T.unpack _graphName
+      &  layout_plots
+      .~ plots
 
 
 drawChart :: TVar Graph -> FlOffscreen -> Ref Widget -> IO ()
