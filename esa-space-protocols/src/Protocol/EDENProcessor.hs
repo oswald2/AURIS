057c5770bed0b5394245c175ed6f25ff4602d55a
Michael Oswald <michael.oswald@onikudaki.net>
2020-12-05 20:26:04 +0100

    Updated to nightly snapshots, some updates to EDEN message handling

--- esa-space-protocols/src/Protocol/EDENProcessor.hs
+++ esa-space-protocols/src/Protocol/EDENProcessor.hs
@@ -32,25 +32,55 @@ edenMessageProcessorC missionSpecific interf = worker HM.empty
   worker counters = do
     x <- await
     case x of
-      Just eden -> case eden ^. edenDataField of
-          -- handle TM via EDEN
-        dat@EdenTM {..} -> do
-          case handleEdenPacket missionSpecific interf dat counters of
-            Left err -> do
-              env <- ask
-              let
-                msg =
-                  display @Text "Error decoding EDEN PUS Packet: " <> display err
-              logDebug msg
-              liftIO $ raiseEvent env $ EVAlarms $ EVPacketAlarm
-                (utf8BuilderToText msg)
-            Right (pkt, newCounters) -> do
-              yield pkt
-              worker newCounters
-        _ -> worker counters
-    
+      Just eden -> do
+        newCounters <- handleEdenMessage missionSpecific interf counters eden
+        worker newCounters
+        worker counters
+
       Nothing -> return ()
 
+
+handleEdenMessage
+  :: (MonadIO m, MonadReader env m, HasRaiseEvent env, HasLogFunc env)
+  => PUSMissionSpecific
+  -> ProtocolInterface
+  -> HashMap VCID Word8
+  -> EdenMessage
+  -> ConduitT EdenMessage ExtractedPacket m (HashMap VCID Word8)
+handleEdenMessage missionSpecific interf counters eden@EdenMessage { _edenType = EdenTMType }
+  = do
+    case eden ^. edenDataField of
+      -- handle TM via EDEN
+      dat@EdenTM {} -> do
+        case handleEdenPacket missionSpecific interf dat counters of
+          Left err -> do
+            env <- ask
+            let msg =
+                  display @Text "Error decoding EDEN PUS Packet: " <> display err
+            logDebug msg
+            liftIO $ raiseEvent env $ EVAlarms $ EVPacketAlarm
+              (utf8BuilderToText msg)
+            return counters
+          Right (pkt, newCounters) -> do
+            yield pkt
+            return newCounters
+      _ -> return counters
+handleEdenMessage _missionSpecific _interf counters eden@EdenMessage { _edenType = EdenTCAType }
+  = do
+    let status = eden ^. edenField2
+        tcID   = eden ^. edenField3
+    logDebug $ display @Text "TC-A: RqstID: " <> display tcID <> if status == 0
+      then display @Text " OK"
+      else display @Text " Error: " <> display status
+    return counters 
+handleEdenMessage _missionSpecific _interf counters eden@EdenMessage { _edenType = EdenTCEType }
+  = do 
+    logDebug $ display @Text "TC Echo:\n" <> displayShow eden 
+    return counters 
+handleEdenMessage _missionSpecific _interf counters eden = do 
+  logDebug $ display @Text "EDEN Message: Type: " <> displayShow (eden ^. edenType) 
+    <> display @Text " SubType: " <> displayShow (eden ^. edenSubType)
+  return counters
 
 
 handleEdenPacket
