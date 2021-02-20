0703be531b3d99ca67e614a949934d51e586a126
Michael Oswald <michael.oswald@onikudaki.net>
2021-02-20 21:35:44 +0100

    First try with new theme

--- aurisi/src/GUI/Theme.hs
+++ aurisi/src/GUI/Theme.hs
@@ -51,15 +51,23 @@ checkDirectory homeDir = do
       return False
 
 
-setTheme :: IO ()
-setTheme = do
-  homeDir <- getHomeDirectory
-  exists  <- checkDirectory homeDir
-  unless exists $ do
-    extractTheme homeDir
+-- setTheme :: IO ()
+-- setTheme = do
+--   homeDir <- getHomeDirectory
+--   exists  <- checkDirectory homeDir
+--   unless exists $ do
+--     extractTheme homeDir
 
-  provider <- cssProviderGetNamed ("Numix-BLACK-SLATE" :: Text) Nothing 
-  screenGetDefault >>= \case
-    Nothing     -> T.putStrLn "Could not get screen"
-    Just screen -> styleContextAddProviderForScreen screen provider 600
+--   provider <- cssProviderGetNamed ("Numix-BLACK-SLATE" :: Text) Nothing 
+--   screenGetDefault >>= \case
+--     Nothing     -> T.putStrLn "Could not get screen"
+--     Just screen -> styleContextAddProviderForScreen screen provider 600
+
+
+setTheme :: IO() 
+setTheme = do 
+    provider <- Gtk.cssProviderNew
+    Gtk.cssProviderLoadFromPath provider "style.css"
+    Just screen <- screenGetDefault
+    Gtk.styleContextAddProviderForScreen screen provider (fromIntegral Gtk.STYLE_PROVIDER_PRIORITY_USER)
 
