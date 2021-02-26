7a5a548ea62189c744fb4dc0d957646b9d6a1dff
Michael Oswald <michael.oswald@onikudaki.net>
2021-02-26 21:28:10 +0100

    Changed files to use resources

--- aurisi/src/GUI/Logo.hs
+++ aurisi/src/GUI/Logo.hs
@@ -13,24 +13,34 @@ import           RIO
 --import qualified Data.Text.IO                  as T
 import           GI.Gtk                        as Gtk
 import           GI.GdkPixbuf.Objects.Pixbuf
-import           GI.GdkPixbuf.Enums
+--import           GI.GdkPixbuf.Enums
 
-import           GUI.LogoXPM
+--import           GUI.LogoXPM
 
+
+-- setLogo :: Image -> Int32 -> Int32 -> IO ()
+-- setLogo img width height = do
+--   pixbuf <- pixbufNewFromXpmData aurisLogoXPM
+--   res    <- pixbufScaleSimple pixbuf width height InterpTypeHyper
+--   case res of
+--     Just p  -> imageSetFromPixbuf img (Just p)
+--     Nothing -> error "Could not scale logo!"
 
 setLogo :: Image -> Int32 -> Int32 -> IO ()
 setLogo img width height = do
-  pixbuf <- pixbufNewFromXpmData aurisLogoXPM
-  res    <- pixbufScaleSimple pixbuf width height InterpTypeHyper
-  case res of
-    Just p  -> imageSetFromPixbuf img (Just p)
-    Nothing -> error "Could not scale logo!"
+  pixbuf <- pixbufNewFromResourceAtScale "/auris/src/AurisLogo.svg" width height True
+  imageSetFromPixbuf img (Just pixbuf)
 
+
+
+-- getLogoPixbuf :: Int32 -> Int32 -> IO Pixbuf
+-- getLogoPixbuf width height = do
+--   pixbuf <- pixbufNewFromXpmData aurisLogoXPM
+--   res    <- pixbufScaleSimple pixbuf width height InterpTypeHyper
+--   case res of
+--     Just p  -> return p
+--     Nothing -> error "Could not scale logo!"
 
 getLogoPixbuf :: Int32 -> Int32 -> IO Pixbuf
 getLogoPixbuf width height = do
-  pixbuf <- pixbufNewFromXpmData aurisLogoXPM
-  res    <- pixbufScaleSimple pixbuf width height InterpTypeHyper
-  case res of
-    Just p  -> return p
-    Nothing -> error "Could not scale logo!"
+  pixbufNewFromResourceAtScale "/auris/src/AurisLogo.svg" width height True
