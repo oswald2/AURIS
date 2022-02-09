07e92902d912d7701e47309155b0b07fdd6a6bfa
Michael Oswald <michael.oswald@onikudaki.net>
2022-02-09 19:11:11 +0100

    Updated to LTS-18.14

--- lib-aurisi/src/GUI/About.hs
+++ lib-aurisi/src/GUI/About.hs
@@ -29,22 +29,25 @@ createAboutDialog = do
     -- setup about dialog
   aboutDialog <- aboutDialogNew
 
-  logo        <- getLogoPixbuf 150 150
+  logo'       <- getLogoPixbuf 150 150
 
-  Gtk.set
-    aboutDialog
-    [ aboutDialogProgramName := ("AURISi" :: Text)
-    , aboutDialogVersion := aurisVersion
-    , aboutDialogCopyright := ("(C) by Michael Oswald" :: Text)
-    , aboutDialogComments
-      := ("AURIS: an open source mission control system.\n\n" :: Text)
-    , aboutDialogAuthors
-      := ["Michael Oswald (michael.oswald@onikudaki.net)" :: Text,
-      "Paolo Varonelli", "Matthias Putz", "Max Taldykin", "Adam Bratschi-Kaye", "Jan van Brüggen"]
-    , aboutDialogLicense := license
-    , aboutDialogWrapLicense := True
-    , aboutDialogLogo := logo
-    ]
+  let attribs1 = 
+        [ aboutDialogProgramName := ("AURISi" :: Text)
+        , aboutDialogVersion := aurisVersion
+        , aboutDialogCopyright := ("(C) by Michael Oswald" :: Text)
+        , aboutDialogComments
+          := ("AURIS: an open source mission control system.\n\n" :: Text)
+        , aboutDialogAuthors
+          := ["Michael Oswald (michael.oswald@onikudaki.net)" :: Text,
+          "Paolo Varonelli", "Matthias Putz", "Max Taldykin", "Adam Bratschi-Kaye", "Jan van Brüggen"]
+        , aboutDialogLicense := license
+        , aboutDialogWrapLicense := True
+        ]
+      attribs = case logo' of 
+          Nothing -> attribs1 
+          Just logo -> attribs1 ++ [aboutDialogLogo := logo]
+
+  Gtk.set aboutDialog attribs
 
   return aboutDialog
 
